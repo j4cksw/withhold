3129a19560fa973f509ae14ed4a3bf7fa3b95adc
Prayoch Rujira <jay@odd-e.co.th>
2017-02-08 11:19:06 +0700

    Reponse as JSON

--- src/server.go
+++ src/server.go
@@ -3,13 +3,23 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"encoding/json"
 )
 
 func main() {
 	fmt.Println("Started")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("AAAA"))
+
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		b, _ := json.Marshal(map[string]int{"value": 30})
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(b))
 	})
 
 	http.ListenAndServe(":3333", nil)
