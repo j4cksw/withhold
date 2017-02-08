7e19ff1d98cc216bd627f9105cf7421e938031f2
Prayoch Rujira <jay@odd-e.co.th>
2017-02-08 12:17:43 +0700

    Ignore files

--- robot/withhold.robot
+++ robot/withhold.robot
@@ -4,12 +4,12 @@ Library    RequestsLibrary
 *** Test cases ***
 Return calculation withold rate of given amount
     Create Session    service    http://localhost:3333
-    ${resp}=	Get Request    service    /withold/1000
+    ${resp}=	Get Request    service    /withold/amount/1000
     Should Be Equal As Strings    ${resp.status_code}    200
     Should Be Equal As Strings    ${resp.json()['value']}    30
 
 Return calculation withold rate of given amount
     Create Session    service    http://localhost:3333
-    ${resp}=	Get Request    service    /withold/10000
+    ${resp}=	Get Request    service    /withold/amount/10000
     Should Be Equal As Strings    ${resp.status_code}    200
     Should Be Equal As Strings    ${resp.json()['value']}    300
--- src/server.go
+++ src/server.go
@@ -4,24 +4,50 @@ import (
 	"net/http"
 	"fmt"
 	"encoding/json"
+	"github.com/gorilla/mux"
+	"strconv"
+	"gopkg.in/mgo.v2"
+	"log"
 )
+
+type Rate struct {
+	Rate float64 `bson:"rate"`
+}
 
 func main() {
 	fmt.Println("Started")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter().StrictSlash(true)
+
+	session, err := mgo.Dial("127.0.0.1:27017")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer session.Close()
+
+	router.HandleFunc("/withold/amount/{amount}", func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Method != "GET" {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
 
-		b, _ := json.Marshal(map[string]int{"value": 30})
+		vars := mux.Vars(r)
+		amount, _ := strconv.ParseFloat(vars["amount"], 64)
+
+		withHold := Rate{}
+
+		collection := session.DB("taxes").C("withold")
+		collection.Find(nil).One(&withHold)
+
+
+		b, _ := json.Marshal(map[string]float64{"value": amount* withHold.Rate})
 
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(b))
 	})
 
-	http.ListenAndServe(":3333", nil)
+	http.ListenAndServe(":3333", router)
 
 }
